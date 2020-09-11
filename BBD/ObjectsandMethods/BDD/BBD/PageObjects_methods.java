ac0f5ee70d7a5338c5a02b31e68476f195fc7d2c
jack733 <71131387+jack733@users.noreply.github.com>
2020-09-11 17:17:36 +0530

    Add files via upload

---
+++ BBD/ObjectsandMethods/BDD/BBD/PageObjects_methods.java
@@ -0,0 +1,47 @@
+package BDD.BBD;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PageObjects_methods {
+
+    public static WebDriver driver;
+
+    
+ // Browser Properties
+  
+    public static void StartBrowserProprties() {
+    	System.setProperty("webdriver.gecko.driver","C:\\Xiaomi\\geckodriver-v0.27.0-win64/geckodriver.exe");
+    	driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	driver.get("https://www.ixigo.com/flights");    
+    	}
+    
+ //Search Flight
+    public static void search() throws InterruptedException {
+    driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+ driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[6]/div/div/div[1]/div/div[1]/input")).sendKeys("New Delhi");
+ driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[6]/div/div/div[1]/div/div[1]/input")).sendKeys(Keys.ENTER); 
+
+Thread.sleep(5000);
+
+driver.findElement(By.xpath("//div[@class='dstn u-ib u-v-align-bottom u-text-left']//input[@placeholder='Enter city or airport']")).sendKeys("Hyderabad");
+
+Thread.sleep(5000);
+driver.findElement(By.xpath("//div[@class='dstn u-ib u-v-align-bottom u-text-left']//input[@placeholder='Enter city or airport']")).sendKeys(Keys.ENTER); 
+    	
+driver.findElement(By.xpath("//button[contains(text(),'Search')]")).click();
+    }
+
+    // Quit Driver
+    public static void Quit(){
+    	driver.quit();  
+    }
+
+    
+}
